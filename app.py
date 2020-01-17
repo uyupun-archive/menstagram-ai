e4bdf3a2414322841b31a98efa9586856b295de6
Kazuki Tobita <kazukiti201@gmail.com>
2020-01-17 19:43:42 +0900

    Add /api/v1/ramen/judge

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({
-        'message': 'hello, world!'
-    })
+    return 'Menstagram AI'
+
+@app.route('/api/v1/ramen/judge')
+def ramen_judge():
+    return jsonify([
+        True, True, True, True,
+    ])
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 80, debug = True)
