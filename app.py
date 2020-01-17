526f310d34ad69804df3f281c6ddaca8b41054e9
Kazuki Tobita <kazukiti201@gmail.com>
2020-01-17 20:58:58 +0900

    Add log settings

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import logging.handlers
+
 from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+handler = logging.handlers.RotatingFileHandler('logs/flask.log', 'a+', maxBytes = 3000, backupCount = 5)
+handler.setFormatter(logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s'))
+app.logger.addHandler(handler)
 
 @app.route('/')
 def index():
@@ -8,6 +14,11 @@ def index():
 
 @app.route('/api/v1/ramen/judge')
 def ramen_judge():
+    app.logger.debug('debug')
+    app.logger.info('info')
+    app.logger.warn('warn')
+    app.logger.error('error')
+    app.logger.critical('critical')
     return jsonify([
         True, True, True, True,
     ])
