99634e16cbe4f482e1f0871d4b3848bed07a7894
Kazuki Tobita <kazukiti201@gmail.com>
2020-01-21 06:22:45 +0900

    Add test_model.py

--- ml/test_model.py
+++ ml/test_model.py
@@ -4,7 +4,6 @@ import keras.preprocessing.image as Image
 
 from keras.models import load_model
 from keras.applications.vgg16 import preprocess_input, decode_predictions
-from keras.preprocessing.image import ImageDataGenerator
 
 model = load_model('judge-ramen-model.h5')
 
@@ -14,7 +13,14 @@ test_data_paths = [
     './learn_data/test/_other/',
 ]
 
-for test_data_path in test_data_paths:
+for i, test_data_path in enumerate(test_data_paths):
+    if i == 0:
+        print('ramen')
+    elif i == 1:
+        print('sutaba')
+    else:
+        print('other')
+
     images = os.listdir(test_data_path)
     for image in images:
         image_path = test_data_path + image
@@ -22,11 +28,12 @@ for test_data_path in test_data_paths:
         x = Image.img_to_array(image)
         x = np.expand_dims(x, axis = 0)
         x = preprocess_input(x)
-        res = model.predict(x, 1)[0]
-        for i, score in enumerate(res):
-            if i == 0:
-                print('{}: {:.2%} '.format('_other', score), end = '')
-            elif i == 1:
-                print('{}: {:.2%} '.format('_ramen', score), end = '')
+        ans = model.predict(x, 1)[0]
+
+        for j, score in enumerate(ans):
+            if j == 0:
+                print('{}: {:.2%} '.format('other', score), end = '')
+            elif j == 1:
+                print('{}: {:.2%} '.format('ramen', score), end = '')
             else:
-                print('{}: {:.2%} '.format('_sutaba', score))
+                print('{}: {:.2%} '.format('sutaba', score))
