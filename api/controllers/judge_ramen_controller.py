c60fac9971ea0121a1bd89dd014b0da58eedb05e
Kazuki Tobita <kazukiti201@gmail.com>
2020-02-04 07:27:05 +0900

    Modify trivial

--- api/controllers/judge_ramen_controller.py
+++ api/controllers/judge_ramen_controller.py
@@ -12,15 +12,14 @@ def predict():
     x = Image.img_to_array(image)
     x = np.expand_dims(x, axis = 0)
     x = preprocess_input(x)
-    # ans = model.predict(x, 1)[0]
-    # other = ans[0]
-    # ramen = ans[1]
-    return []
+    ans = model.predict(x, 1)[0]
+    other = ans[0]
+    ramen = ans[1]
+    res = True if ramen > other else False
+    return res
 
 @app.route('/api/v1/ramen/judge', methods = ['POST'])
 def judge_ramen():
-    res = predict()
-
     return jsonify([
-        res
+        predict()
     ])
