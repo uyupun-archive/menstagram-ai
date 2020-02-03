83799eb06e9a3623d33edadad86a509df6fbc671
Kazuki Tobita <kazukiti201@gmail.com>
2020-02-04 06:32:35 +0900

    Modify judge ramen

--- api/__init__.py
+++ api/__init__.py
@@ -8,7 +8,7 @@ app.config.from_object('api.config')
 
 logging.basicConfig(filename = 'api/logs/flask.log', format = '[%(asctime)s] %(levelname)s: %(message)s', level = app.config['LOG_LEVEL'])
 
-# model = load_model('../trainer/judge-ramen-model.h5')
-# model._make_predict_function()
+model = load_model('trainer/judge-ramen-model.h5')
+model._make_predict_function()
 
 from api.controllers import index_controller, judge_ramen_controller
--- api/controllers/judge_ramen_controller.py
+++ api/controllers/judge_ramen_controller.py
@@ -2,18 +2,20 @@ import numpy as np
 import keras.preprocessing.image as Image
 
 from flask import jsonify
+from keras.applications.vgg16 import preprocess_input
 from api import app
+from api import model
 
 def predict():
     image_path = './trainer/learn_data/test/_ramen/663.jpg'
-    # image = Image.load_img(image_path, target_size = (224, 224))
-    # x = Image.img_to_array(image)
-    # x = np.expand_dims(x, axis = 0)
-    # x = preprocess_input(x)
+    image = Image.load_img(image_path, target_size = (224, 224))
+    x = Image.img_to_array(image)
+    x = np.expand_dims(x, axis = 0)
+    x = preprocess_input(x)
     # ans = model.predict(x, 1)[0]
     # other = ans[0]
     # ramen = ans[1]
-    return True
+    return []
 
 @app.route('/api/v1/ramen/judge', methods = ['POST'])
 def judge_ramen():
