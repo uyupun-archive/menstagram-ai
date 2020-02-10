14d754519656de977b8e35c01b367b5a636aa426
Kazuki Tobita <kazukiti201@gmail.com>
2020-02-11 02:13:14 +0900

    Fix download_learn_data.sh

--- trainer/download_learn_data.sh
+++ trainer/download_learn_data.sh
@@ -8,8 +8,8 @@ if [ ! -e 'chromedriver' ];then
   echo "Download from http://chromedriver.chromium.org/downloads"
   echo "Then move 'chromedriver' file to 'trainer' directory."
 else
-  googleimagesdownload -ri -cd "./chromedriver" -k "ラーメン" -f "jpg" -l 1000
-  googleimagesdownload -ri -cd "./chromedriver" -k "画像" -f "jpg" -l 1000
-  googleimagesdownload -ri -cd "./chromedriver" -k "写真" -f "jpg" -l 1000
-  googleimagesdownload -ri -cd "./chromedriver" -k "静止画" -f "jpg" -l 1000
+  googleimagesdownload -ri -cd "trainer/chromedriver" -k "ラーメン" -f "jpg" -l 1000
+  googleimagesdownload -ri -cd "trainer/chromedriver" -k "画像" -f "jpg" -l 1000
+  googleimagesdownload -ri -cd "trainer/chromedriver" -k "写真" -f "jpg" -l 1000
+  googleimagesdownload -ri -cd "trainer/chromedriver" -k "静止画" -f "jpg" -l 1000
 fi
